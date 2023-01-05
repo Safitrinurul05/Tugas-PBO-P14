d97959ff284b36bdd1f6a7b1632cf54af38179cf
Safitrinurul05 <113685432+Safitrinurul05@users.noreply.github.com>
2023-01-05 19:37:31 +0700

    Add files via upload

---
+++ data_warga.java
@@ -0,0 +1,59 @@
+public class data_warga{
+   String nama,nik,alamat,no_telp,jenis_pekerjaan,alamat_kantor,status,nama_pasangan;
+   Integer jml_anak;
+
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getNik() {
+        return nik;
+    }
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+    public String getAlamat() {
+        return alamat;
+    }
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    public String getNoTelp() {
+        return no_telp;
+    }
+    public void setNoTelp(String noTelp) {
+        this.no_telp = noTelp;
+    }
+    public String getJenis_pekerjaan() {
+        return jenis_pekerjaan;
+    }
+    public void setJenis_pekerjaan(String jenis_pekerjaan) {
+        this.jenis_pekerjaan = jenis_pekerjaan;
+    }
+     public String getAlamat_kantor() {
+        return alamat_kantor;
+    }
+    public void setAlamat_kantor(String alamat_kantor) {
+        this.alamat_kantor = alamat_kantor;
+    }
+     public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        status = status;
+    }
+     public String getNamaPasangan() {
+        return nama_pasangan;
+    }
+    public void setNamaPasangan(String namaPasangan) {
+        this.nama_pasangan = namaPasangan;
+    }
+    public Integer getJml_anak() {
+        return jml_anak;
+    }
+    public void setJml_anak(Integer jml_anak) {
+        this.jml_anak = jml_anak;
+    }
+}
